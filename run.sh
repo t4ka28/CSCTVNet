ad30245b417e561e5ef784ef34af89c6b3c43020
t4ka28 <ueki.t.ac@m.titech.co.jp>
2024-12-19 12:46:17 +0900

    反映させない項目を追加

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source ../../anaconda3/etc/profile.d/conda.sh
 conda activate MyResearch
-python run.py --n_ch 1 --model_tag Test
+python train.py --n_ch 1 --blur_level 3 --noise_level 1e-1 --num_filters 3  --num_epochs 500 --model_tag Ablation_k3
+python train.py --n_ch 1 --blur_level 3 --noise_level 1e-1 --num_filters 5  --num_epochs 500 --model_tag Ablation_k5
+python train.py --n_ch 1 --blur_level 3 --noise_level 1e-1 --num_filters 7  --num_epochs 500 --model_tag Ablation_k7
+python train.py --n_ch 1 --blur_level 3 --noise_level 1e-1 --num_filters 9  --num_epochs 500 --model_tag Ablation_k9
+python train.py --n_ch 1 --blur_level 3 --noise_level 1e-1 --num_filters 11 --num_epochs 500 --model_tag Ablation_k11
