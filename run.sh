6cb0c6eb887be764b8d5e533e6da51c8df26e2a4
t4ka28 <taka.ueki.0428@gmail.com>
2025-01-10 12:47:31 +0900

    削除

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ../../anaconda3/etc/profile.d/conda.sh
-conda activate Myresearch
-
-python train.py --n_ch 3 --deg inpainting --missing_rate 10 --noise_level 0 --num_epochs 500 --model_tag 提案手法
-python train.py --n_ch 3 --deg inpainting --missing_rate 30 --noise_level 0 --num_epochs 500 --model_tag 提案手法
-python train.py --n_ch 3 --deg inpainting --missing_rate 50 --noise_level 0 --num_epochs 500 --model_tag 提案手法
